a6fff7601d81ffc8f51043164eb689876d53c616
Abu Naser Muhammad Arman <arman.rajbari@gmail.com>
2024-10-26 02:25:50 +0600

    Made the function to get and copy quote

--- js/script.js
+++ js/script.js
@@ -1,20 +1,35 @@
-const url = 'https://api.api-ninjas.com/v1/quotes';
-fetch(url, {
-  method: 'GET',
-  headers: {
-    'X-Api-Key': '2GSbkhrXIVJneFC6ZqhFLg==Lsv2Trh48ZLiTg4C',
-    'Content-Type': 'application/json'
-  }
-})
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-    const randomQuote = data[0];  // because the response was given in array by api-ninja, and the first one is our data
-
-    document.getElementById('author').textContent = randomQuote.author;
-    document.querySelector('.category').textContent = randomQuote.category;
-    document.getElementById('quote').textContent = randomQuote.quote;
+// Fetch a quote from Ninja-API
+function getQuote() {
+  fetch('https://api.api-ninjas.com/v1/quotes', {
+    method: 'GET',
+    headers: {
+      'X-Api-Key': '2GSbkhrXIVJneFC6ZqhFLg==Lsv2Trh48ZLiTg4C',
+      'Content-Type': 'application/json'
+    }
   })
-  .catch(error => {
-    console.error('Error fetching quotes:', error);
-  });
+    .then(response => response.json())
+    .then(data => {
+      if (data && data.length > 0) {
+        const fetchedQuote = data[0];
+
+        // Update HTML elements with fetched quote
+        document.getElementById('author').textContent = fetchedQuote.author;
+        document.querySelector('.category').textContent = fetchedQuote.category;
+        document.getElementById('quote').textContent = fetchedQuote.quote;
+      } else {
+        console.error('No quote received');
+      }
+    })
+    .catch(error => {
+      console.error('Error fetching quotes:', error);
+    });
+}
+
+// To generate a new quote on page load 
+getQuote();
+
+// Copy the quote to clipboard
+function copyQuote() {
+  navigator.clipboard.writeText(`"${document.getElementById('quote').textContent}" 
+- ${document.getElementById('author').textContent}`);
+}
